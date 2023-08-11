42d27213473324d5fb9e6f6a16b55a2dd8178afb
Charity <umercy001@gmail.com>
2023-08-11 13:46:23 +0100

    added the signIn component

--- src/routes/signIn/signIn.component.jsx
+++ src/routes/signIn/signIn.component.jsx
@@ -1,5 +1,20 @@
+import { Fragment } from "react"
+import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils"
+
 export const SignIn = () => {
+
+    const logGoogleUser = async() => {
+        const response = await signInWithGooglePopup();
+        console.log(response)
+    };
+
     return (
-        <h1> Just sign In</h1>
+        <Fragment>
+          <h1> Just sign In</h1>
+        <button onClick={logGoogleUser}>
+            sign in with Google
+        </button>  
+        </Fragment>
+        
     )
 }
